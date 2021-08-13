eca2f688bf40be61e1e812ee953e9a344d68612f
Louis Lu <louislu705@gmail.com>
2021-08-13 18:13:01 -0400

    Customer.java added

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,6 +1,21 @@
 package src.main.java;
 
+import java.util.LinkedList;
+
 public class Customer
 {
+	private LinkedList<OrderItem> allOrders;
 	
+	public Customer()
+	{
+
+	}
+	public void placeOrder(OrderItem order)
+	{
+		allOrders.add(order);
+	}
+	public void cancelOrder(int index)
+	{
+		allOrders.remove(index);
+	}
 }
