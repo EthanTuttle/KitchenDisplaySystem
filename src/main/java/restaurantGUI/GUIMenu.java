c5ebcdd0c37a2799df3de4c4d0b269a766a959d5
Ethan Tuttle <ethantut@gmail.com>
2021-08-13 18:38:15 -0400

    gui started

---
+++ src/main/java/restaurantGUI/GUIMenu.java
@@ -0,0 +1,11 @@
+package src.main.java.restaurantGUI;
+
+import javax.swing.JMenuBar;
+
+public class GUIMenu  extends JMenuBar {
+    
+    public GUIMenu(RestuarantGUI gui) {
+        
+    }
+
+}
--- src/main/java/restaurantGUI/RestuarantGUI.java
+++ src/main/java/restaurantGUI/RestuarantGUI.java
@@ -3,15 +3,27 @@ package src.main.java.restaurantGUI;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.JFrame;
-import java.awt.event.*;
-import java.awt.event.EventQueue;
 
 public class RestuarantGUI extends JFrame{
-    EventQueue.invokeLater(new Runnable()
-    {
-        public void run()
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable()
         {
-            
-        }
-    });
+            public void run()
+            {
+                new RestuarantGUI();
+            }
+        });
+    }
+
+    public RestuarantGUI() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setJMenuBar(new GUIMenu(this));
+        setTitle("Epidemic Simulation");
+
+        setVisible(true);
+        setExtendedState(getExtendedState() | MAXIMIZED_BOTH);
+    }
+
 }
