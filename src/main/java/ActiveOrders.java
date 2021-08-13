994f4c4b2e883f9fbcb41aeb67edea1a00081a27
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2021-08-13 18:31:16 -0400

    Active Orders for Busniess

---
+++ src/main/java/ActiveOrders.java
@@ -0,0 +1,23 @@
+package src.main.java;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class ActiveOrders {
+    private PriorityQueue<Customer> orders;
+
+    public ActiveOrders(Customer aCustomer) {
+
+        orders = new PriorityQueue<Customer>();
+
+        orders.add(aCustomer);
+
+    }
+
+    public void ArrangeQueue() {
+    }
+
+    public void displayQueue() {
+    }
+
+}
