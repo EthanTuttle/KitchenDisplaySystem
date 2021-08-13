497ade02e105044459a098a43b5a55682f664129
Shawn <georgs2@rpi.edu>
2021-08-13 19:58:32 -0400

    Update menu to include categories

--- src/main/java/MenuItem.java
+++ src/main/java/MenuItem.java
@@ -4,12 +4,10 @@ public class MenuItem {
     
     private String name; //name of menu item
     private int timeToMake; //measured in ms
-    private int menu_id; //id for menu item
 
-    public MenuItem(String name, int ms, int id) {
+    public MenuItem(String name, int ms) {
         this.name = name;
         this.timeToMake = ms;
-        this.menu_id = id;
     }
 
     public String getName() {
@@ -18,9 +16,6 @@ public class MenuItem {
 
     public int getTimeToMake() {
         return timeToMake;
-    }
-    public int getId(){
-        return menu_id;
     }
 
 }
