5984ebb7da57945ac8cefcb9e5e01a31c47fe960
Shawn <georgs2@rpi.edu>
2021-08-15 17:49:17 -0400

    Fixing comparator

--- src/main/java/Backend/Customer.java
+++ src/main/java/Backend/Customer.java
@@ -112,4 +112,8 @@ public class Customer
 	public void incrementTick(){
 		ticksInQueue++;
 	}
+
+	public int getTick(){
+		return ticksInQueue;
+	}
 }
--- src/main/java/Backend/OrderComparator.java
+++ src/main/java/Backend/OrderComparator.java
@@ -4,12 +4,22 @@ import java.util.Comparator;
 
 public class OrderComparator implements Comparator<Customer> {
     public int compare(Customer c1, Customer c2){
-        if (c1.getTimeToMake()<c2.getTimeToMake()){
+        if ((c1.getTick()>=5 && c2.getTick()>=5) || (c1.getTick()<5 && c2.getTick()<5)){
+            if (c1.getTimeToMake()<c2.getTimeToMake()){
+                return -1;
+            }
+            else if (c1.getTimeToMake()>c2.getTimeToMake()){
+                return 1;
+            }
+            return 0;
+        }
+        else if (c1.getTick()>=5 && c2.getTick()<5){
             return -1;
         }
-        else if (c1.getTimeToMake()>c2.getTimeToMake()){
+        else if (c1.getTick()<5 && c2.getTick()>=5){
             return 1;
         }
+
         return 0;
     }
 }
