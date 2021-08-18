e60c12f03d43b807d3825a208fd672d55cf3d68a
JohnF1103 <87583884+JohnF1103@users.noreply.github.com>
2021-08-18 17:45:40 -0400

    new GUI

--- src/main/java/customerGUI/CustomerGUI.java
+++ src/main/java/customerGUI/CustomerGUI.java
@@ -1,35 +1,127 @@
 package src.main.java.customerGUI;
-
-import java.awt.*;
-import javax.swing.*;
-
-import src.main.java.Backend.Connection;
-
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CustomerGUI extends JFrame {
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-                new CustomerGUI();
-            }
-        });
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+
+public class CustomerGUI implements ActionListener ,ChangeListener {
+
+    JButton button;
+    JButton buttonmainCourse;
+    JButton buttonSides;
+    JButton buttonDrinks;
+    JButton buttonAPPs;
+    
+
+
+    JFrame frame;
+
+    public CustomerGUI(){
+
+         frame = new JFrame("Demo Frame");
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+  
+        JPanel panel = new JPanel();
+        frame.getContentPane();
+  
+
+
+        JPanel buttonPanel = new JPanel();
+
+      button = new JButton("Checkout");
+      buttonAPPs = new JButton("Apps");
+      buttonmainCourse = new JButton("Main Courses");
+
+      buttonSides = new JButton("Sides");
+      buttonDrinks = new JButton("Drinks");
+
+      button.setActionCommand("Checkout");
+
+
+      button.addActionListener(this);
+
+  
+  
+  
+        Dimension size = button.getPreferredSize();
+  
+        button.setBounds(1200, 730, size.width +30, size.height+30 ) ;
+        button.setBackground(Color.green);
+        button.setOpaque(true);
+
+
+
+        buttonPanel.add(buttonmainCourse);
+        buttonPanel.add(buttonSides);
+        buttonPanel.add(buttonDrinks);
+        buttonPanel.add(buttonAPPs);
+
+        panel.setLayout(null);
+        panel.add(button);
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        frame.add(buttonPanel);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(panel);
+        frame.setSize(500, 300);
+        frame.setVisible(true);
     }
 
-    private Connection connection;
 
-    public void CustomerGUI() {
-        setSize(500, 500);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setJMenuBar(new GUIMenu(this));
-        setTitle("Customer View");
+    public void init(Container container){};
 
-        setVisible(true);
-        setExtendedState(getExtendedState() | MAXIMIZED_BOTH);
+
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        
     }
+    
+    
+    
+    
+    
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
+        String command = e.getActionCommand();
+        if (command.equals("Checkout")) {
+
+            System.out.println("ORDER PLACED");
+          }
+        
+    }
+    
+    
+
+   public static void main(String[] args) {
+     
+        CustomerGUI g = new CustomerGUI();
+        
+
+      
+   }
+
+
+
+
+
+
+
+
+   
 }
