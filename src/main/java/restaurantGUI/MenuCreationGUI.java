5bb94ff6c37bc920e3c7838cb9d9ef4c22d5b645
Louis Lu <louislu705@gmail.com>
2021-08-19 00:27:59 -0400

    Developmental commit

--- src/main/java/restaurantGUI/MenuCreationGUI.java
+++ src/main/java/restaurantGUI/MenuCreationGUI.java
@@ -73,7 +73,6 @@ public class MenuCreationGUI extends JPanel {
         add(scrollPanel,BorderLayout.CENTER);
 
         // If a menu exists than load it instead of showing an empty menu
-        /*
         loadingMenu = true;
         Iterator<String> itr1 = menu.allItems().keySet().iterator();
         while (itr1.hasNext()) {
@@ -88,7 +87,7 @@ public class MenuCreationGUI extends JPanel {
                 itemButtons.get(itemButtons.size()-1).getActionListeners()[0].actionPerformed(null);
             }
         }
-        loadingMenu = false;*/
+        loadingMenu = false;
     }
     public boolean checkValidValue(String value, String type){
         if (type.equals("category")){
