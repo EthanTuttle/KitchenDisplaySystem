a02798e746d08fe59dd2109f767c68348e7d942d
Ethan Tuttle <ethantut@gmail.com>
2021-08-19 16:56:29 -0400

    redid the customer information

--- src/main/java/customerGUI/CustomerGUI.java
+++ src/main/java/customerGUI/CustomerGUI.java
@@ -2,7 +2,8 @@ package src.main.java.customerGUI;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.ImageIcon;
+import java.awt.EventQueue;
+import java.awt.event.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.concurrent.Semaphore;
@@ -39,65 +40,86 @@ public class CustomerGUI {
      * Instantiate CustomerGUI
      */
     public CustomerGUI() {
-        
-        //create jdialog for input
-        JPanel input = new JPanel();
-        input.setLayout(new GridLayout(0, 2));
-        input.add(new JLabel("Name: "));
-        JTextField nameInput = new JTextField();
-        input.add(nameInput);
-        input.add(new JLabel("IP: "));
-        JTextField ipInput = new JTextField();
-        input.add(ipInput);
-        int result = JOptionPane.showConfirmDialog(null, input, "Enter the information to run the program", JOptionPane.OK_CANCEL_OPTION);
 
-        //pull results
-        if (result == JOptionPane.OK_OPTION) {
-            if (nameInput.getText().length() == 0 || ipInput.getText().length() == 0 ) {
-                JOptionPane.showMessageDialog(null, "Must enter all inputs", "Input Error", JOptionPane.ERROR_MESSAGE);
-            } else {
-                rpiIP = ipInput.getText();
-                try {
-                    loadSemaphore.acquire();
-                    connection = new Socket(InetAddress.getByName(rpiIP), 55555);
-                    in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    new Thread(new Runnable() {
-                        public void run() {
-                            menu = new src.main.java.Backend.Menu();
-                            while (connection.isConnected()) {
-                                try {
-                                    String line = in.readLine();
-                                    if (line.equals("End of Menu")) {
-                                        break;
-                                    } else { //On connection host should send all menu items as name;timeToMake;category
-                                        String[] list = line.split(";");
-                                        if (!menu.containsCategory(list[2])) {
-                                            menu.addCategory(list[2]);
-                                        }
-                                        menu.addMenuItem(list[2], list[0],  Integer.parseInt(list[1]));
-                                    }
-                                } catch (IOException e) {
-                                    //TODO: 
-                                    System.out.println(e);
-                                }
-                                
-                            }
-                            loadSemaphore.release();
-                        }
-                    }).start();
-                    loadSemaphore.acquire();
-                    new Mainframe(menu, nameInput.getText(), connection);
-                    loadSemaphore.release();
-                } catch (IOException e) {
-                    JOptionPane.showMessageDialog(null, e, "Connection Error", JOptionPane.ERROR_MESSAGE);
-                } catch (InterruptedException e) {
-                    JOptionPane.showMessageDialog(null, e, "Thread Error", JOptionPane.ERROR_MESSAGE);
-                }
-                
-            }
+        JFrame options = new JFrame();
+        ImageIcon ninjaIcon = null;
+        java.net.URL imgURL = CustomerGUI.class.getResource("ninja.png");
+        if (imgURL != null) {
+            ninjaIcon = new ImageIcon(imgURL);
+            options.setIconImage(ninjaIcon.getImage());
+        } else {
+            JOptionPane.showMessageDialog(null, "Icon image not found.");
         }
+        options.setTitle("Customer Information");
+        options.setSize(350, 125);
+        options.setLocationRelativeTo(null);
+        JPanel optionsPanelWrapper = new JPanel();
+        optionsPanelWrapper.setLayout(new FlowLayout());
+        JPanel optionsPanel = new JPanel();
+        optionsPanel.setLayout(new GridLayout(0, 2));
+        optionsPanel.add(new JLabel("Enter Name: ", SwingConstants.CENTER));
+        JTextField nameInput = new JTextField();
+        nameInput.setColumns(18);
+        optionsPanel.add(nameInput);
+        optionsPanel.add(new JLabel("Enter IP: ", SwingConstants.CENTER));
+        JTextField ipInput = new JTextField();
+        ipInput.setColumns(18);
+        optionsPanel.add(ipInput);
+        optionsPanel.add(new JLabel());
+        JButton submit = new JButton("Submit");
+        submit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                if (nameInput.getText().length() == 0 || ipInput.getText().length() == 0 ) {
+                    JOptionPane.showMessageDialog(null, "Must enter all inputs", "Input Error", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    rpiIP = ipInput.getText();
+                    try {
+                        loadSemaphore.acquire();
+                        connection = new Socket(InetAddress.getByName(rpiIP), 55555);
+                        in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        new Thread(new Runnable() {
+                            public void run() {
+                                menu = new src.main.java.Backend.Menu();
+                                while (connection.isConnected()) {
+                                    try {
+                                        String line = in.readLine();
+                                        if (line.equals("End of Menu")) {
+                                            break;
+                                        } else { //On connection host should send all menu items as name;timeToMake;category
+                                            String[] list = line.split(";");
+                                            if (!menu.containsCategory(list[2])) {
+                                                menu.addCategory(list[2]);
+                                            }
+                                            menu.addMenuItem(list[2], list[0],  Integer.parseInt(list[1]));
+                                        }
+                                    } catch (IOException e) {
+                                        //TODO: 
+                                        System.out.println(e);
+                                    }
+                                    
+                                }
+                                loadSemaphore.release();
+                            }
+                        }).start();
+                        loadSemaphore.acquire();
+                        options.setVisible(false);
+                        new Mainframe(menu, nameInput.getText(), connection);
+                        loadSemaphore.release();
+                    } catch (IOException e) {
+                        JOptionPane.showMessageDialog(null, e, "Connection Error", JOptionPane.ERROR_MESSAGE);
+                    } catch (InterruptedException e) {
+                        JOptionPane.showMessageDialog(null, e, "Thread Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                    
+                }
+            }
+        });
+        optionsPanel.add(submit);
+        options.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        optionsPanelWrapper.add(optionsPanel);
+        options.add(optionsPanelWrapper);
+        options.setVisible(true);
 
-        
     }
     /**
      * Main function creates a new Customer GUI
